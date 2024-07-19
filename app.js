2706b17b6fab807b78c87d951766682b41292e92
alecs <camaradaloradha@hotmail.com>
2024-07-18 19:59:38 -0600

    actualizacion de todo el archivo

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
+document.getElementById('encryptButton').addEventListener('click', function() {
+    const inputText = document.getElementById('inputText').value;
+    const encryptedText = encrypt(inputText);
+    document.getElementById('outputText').value = encryptedText;
+});
+
+document.getElementById('decryptButton').addEventListener('click', function() {
+    const inputText = document.getElementById('inputText').value;
+    const decryptedText = decrypt(inputText);
+    document.getElementById('outputText').value = decryptedText;
+});
+
+document.getElementById('copyButton').addEventListener('click', function() {
+    const outputText = document.getElementById('outputText').value;
+    if (outputText) {
+        navigator.clipboard.writeText(outputText).then(function() {
+            alert('Texto copiado al portapapeles');
+        }, function(err) {
+            alert('Error al copiar el texto');
+        });
+    } else {
+        alert('No hay texto para copiar');
+    }
+    
+
+});
+
+function encrypt(text) {
+    return text.replace(/e/g, 'enter')
+               .replace(/i/g, 'imes')
+               .replace(/a/g, 'ai')
+               .replace(/o/g, 'ober')
+               .replace(/u/g, 'ufat');
+}
+
+function decrypt(text) {
+    return text.replace(/enter/g, 'e')
+               .replace(/imes/g, 'i')
+               .replace(/ai/g, 'a')
+               .replace(/ober/g, 'o')
+               .replace(/ufat/g, 'u');
+}
+
+
+
+function limpiarTodoCampo(){
+    let valorCaja = document.querySelector('#limpiarCampo');
+    valorCaja.value = '';
+
+}
+    
+
 
